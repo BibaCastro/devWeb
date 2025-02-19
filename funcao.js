457603b411b6ba929099f3244f5e29198226eeff
BibaCastro <bealvesx@gmail.com>
2025-02-19 18:04:21 +0000

    tailgatelindo

---
+++ funcao.js
@@ -0,0 +1,3 @@
+function navegar(){
+    window.location = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftvtropes.org%2Fpmwiki%2Fpmwiki.php%2FHeartwarming%2FTheTransformersMoreThanMeetsTheEye&psig=AOvVaw1Y5fbOyAunHfTfGtth0zBb&ust=1740074029257000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOCLr7Km0IsDFQAAAAAdAAAAABAE";
+}
